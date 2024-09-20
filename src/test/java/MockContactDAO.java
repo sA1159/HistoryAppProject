9261bff8c558bfa3a910309114d4f79142495e5e
Sudashna Amarakoon <n11594268@qut.edu.au>
2024-09-20 19:28:59 +1000

    unit test finished

---
+++ src/test/java/MockContactDAO.java
@@ -0,0 +1,65 @@
+import com.example.cab302prac4.model.Contact;
+import com.example.cab302prac4.model.IContactDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockContactDAO implements IContactDAO {
+    /**
+     * A static list of contacts to be used as a mock database.
+     */
+    public static final ArrayList<Contact> contacts = new ArrayList<>();
+    private static int autoIncrementedId = 0;
+
+    public MockContactDAO() {
+        // Add some initial contacts to the mock database
+        addContact(new Contact("a", "b", "c@example.com", "d", "a", "a", "a", 1));
+    }
+
+    @Override
+    public void addContact(Contact contact) {
+        contact.setId(autoIncrementedId);
+        autoIncrementedId++;
+        contacts.add(contact);
+    }
+
+    @Override
+    public void updateContact(Contact contact) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getId() == contact.getId()) {
+                contacts.set(i, contact);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void deleteContact(Contact contact) {
+        contacts.remove(contact);
+    }
+
+    @Override
+    public Contact getContact(int id) {
+        for (Contact contact : contacts) {
+            if (contact.getId() == id) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Contact> getAllContacts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> getAllContactsSearch(String search) {
+        return List.of();
+    }
+
+    @Override
+    public List<Contact> getAllContactsByID(int ID) {
+        return List.of();
+    }
+}
